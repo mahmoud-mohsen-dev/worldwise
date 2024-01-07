2074470a9b2ac30c1b61e748fde91b418c44c1d5
Mahmoud Mohsen <mahmoud.mohsen120@gmail.com>
2024-01-07 15:27:02 +0200

    add starter components

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,23 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
 
 function PageNav() {
     return (
         <nav className={styles.nav}>
+            <Link to="/">
+                <img src="./logo.png" alt="worldwise-logo" />
+            </Link>
             <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
                 <li>
                     <NavLink to="/pricing">Pricing</NavLink>
                 </li>
                 <li>
                     <NavLink to="/product">Product</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/login" className={styles.ctaLink}>
+                        Login
+                    </NavLink>
                 </li>
             </ul>
         </nav>
