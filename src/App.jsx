a5b430529fb545a0f37e85acfc9bb0c3d4392d1b
Mahmoud Mohsen <mahmoud.mohsen120@gmail.com>
2024-01-07 03:21:47 +0200

    adding: the starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
-import Home from "./pages/Home";
+import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="product" element={<Product />} />
                 <Route path="pricing" element={<Pricing />} />
-                <Route path="/" element={<Home />} />
+                <Route path="app" element={<AppLayout />} />
+                <Route path="/" element={<Homepage />} />
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
         </BrowserRouter>
